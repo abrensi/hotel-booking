766a9d6c7a298f99dcca30396ac6b3e89068d892
abrensi <kalkhanovn@mail.ru>
2021-05-30 04:52:08 +0600

    create: validation and animation

--- js/main.js
+++ js/main.js
@@ -65,37 +65,66 @@ $(document).ready(function () {
     spaceBetween: 15,
   });
   
-  var menuButton = $(".menu-button");
+  const menuButton = $(".menu-button");
     menuButton.on("click", function () {
       $(".nav-bottom").toggleClass("nav-bottom--visible");
       $("body").toggleClass("no-scroll");
     });
 
-  var modalButton = $("[data-toggle=modal]");
-  var closeModalButton = $(".modal__close");
+  const modalButton = $("[data-toggle=modal]");
+  const closeModalButton = $(".modal__close");
+
+  const modalOverlay = $('.modal__overlay')
+  const modalDialog = $('.modal__dialog')
+
   modalButton.on('click', openModal);
   closeModalButton.on('click', closeModal);
+  modalOverlay.on('click', closeModal);
 
   function openModal() {
-    var modalOverlay = $('.modal__overlay')
-    var modalDialog = $('.modal__dialog')
     modalOverlay.addClass('modal__overlay--visible');
     modalDialog.addClass('modal__dialog--visible');
+    $("body").addClass("no__scroll");
   }
+
   function closeModal(event) {
     event.preventDefault();
-    var modalOverlay = $('.modal__overlay')
-    var modalDialog = $('.modal__dialog')
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
+    $("body").removeClass("no__scroll");
+    
   }
-});
 
-$(document).keydown(function (e) {
-  if (e.keyCode === 27) {
-    var modalOverlay = $('.modal__overlay')
-    var modalDialog = $('.modal__dialog') 
-    modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
-  }
-});
+  $(document).keydown(function (e) {
+    if (e.keyCode === 27) {
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+      $("body").removeClass("no__scroll");
+    }
+  })
+  AOS.init();
+
+  // Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlenght: "Not shorter than 2 letters!",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "Please specify your phone number",
+        }
+      }
+    })
+  })
+
+  jQuery(document).ready(function() {
+    $('.phone').mask('+7 (999) 999-99-99')
+  })
+})
