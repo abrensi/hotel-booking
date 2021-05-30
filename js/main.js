502f65e3ddadd8d8760f3134432dba7f98206fc5
abrensi <kalkhanovn@mail.ru>
2021-05-31 00:15:43 +0600

    fixed: fixed sections

--- js/main.js
+++ js/main.js
@@ -2,21 +2,21 @@ $(document).ready(function () {
   const hotelSlider = new Swiper(".hotel-slider", {
     // Optional parameters
     loop: true,
-  
+
     // Navigation arrows
     navigation: {
       nextEl: ".hotel-slider__button__next",
       prevEl: ".hotel-slider__button__prev",
     },
-  
+
     keyboard: {
       enabled: true,
       onlyInViewport: false,
     },
-  
+
     spaceBetween: 15,
   });
-  
+
   ymaps.ready(init);
   function init() {
     // Создание карты.
@@ -42,66 +42,65 @@ $(document).ready(function () {
         iconColor: "#EC1F46",
       }
     );
-  
+
     // После того как метка была создана, добавляем её на карту.
     myMap.geoObjects.add(myPlacemark);
   }
-  
+
   const reviewsSlider = new Swiper(".reviews-slider", {
     // Optional parameters
     loop: true,
-  
+
     // Navigation arrows
     navigation: {
       nextEl: ".reviews-slider__button__next",
       prevEl: ".reviews-slider__button__prev",
     },
-  
+
     keyboard: {
       enabled: true,
       onlyInViewport: false,
     },
-  
+
     spaceBetween: 15,
   });
-  
+
   const menuButton = $(".menu-button");
-    menuButton.on("click", function () {
-      $(".nav-bottom").toggleClass("nav-bottom--visible");
-      $("body").toggleClass("no-scroll");
-    });
+  menuButton.on("click", function () {
+    $(".nav-bottom").toggleClass("nav-bottom--visible");
+    $("body").toggleClass("no-scroll");
+  });
 
   const modalButton = $("[data-toggle=modal]");
   const closeModalButton = $(".modal__close");
 
-  const modalOverlay = $('.modal__overlay')
-  const modalDialog = $('.modal__dialog')
+  const modalOverlay = $(".modal__overlay");
+  const modalDialog = $(".modal__dialog");
 
-  modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal);
-  modalOverlay.on('click', closeModal);
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+  modalOverlay.on("click", closeModal);
 
   function openModal() {
-    modalOverlay.addClass('modal__overlay--visible');
-    modalDialog.addClass('modal__dialog--visible');
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
     $("body").addClass("no__scroll");
   }
 
   function closeModal(event) {
     event.preventDefault();
-    modalOverlay.removeClass('modal__overlay--visible');
-    modalDialog.removeClass('modal__dialog--visible');
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
     $("body").removeClass("no__scroll");
-    
   }
 
   $(document).keydown(function (e) {
     if (e.keyCode === 27) {
-      modalOverlay.removeClass('modal__overlay--visible');
-      modalDialog.removeClass('modal__dialog--visible');
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
       $("body").removeClass("no__scroll");
     }
-  })
+  });
   AOS.init();
 
   // Обработка форм
@@ -115,16 +114,29 @@ $(document).ready(function () {
         },
         email: {
           required: "We need your email address to contact you",
-          email: "Your email address must be in the format of name@domain.com"
+          email: "Your email address must be in the format of name@domain.com",
         },
         phone: {
           required: "Please specify your phone number",
-        }
-      }
-    })
-  })
+        },
+      },
+    });
+  });
 
-  jQuery(document).ready(function() {
-    $('.phone').mask('+7 (999) 999-99-99')
-  })
-})
+  jQuery(document).ready(function () {
+    $(".phone").mask("+7 (999) 999-99-99");
+  });
+
+  $(function() {
+    $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {     
+    $('#toTop').fadeIn();     
+    } else {     
+    $('#toTop').fadeOut();     
+    }     
+    });    
+    $('#toTop').click(function() {    
+    $('body,html').animate({scrollTop:0},800);   
+    });  
+    });
+});
